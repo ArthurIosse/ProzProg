c02f01de14a87d5cd059f2cc9b63429a47044a45
Arthur <iosse.arthur@gmail.com>
2018-11-18 23:21:32 +0100

    new p

--- blatt4/p1.c
+++ blatt4/p1.c
@@ -4,7 +4,28 @@
 int main (int argc, char * argv[])
 {
 
-  
-printf("p1\n");
+double d1, d2;
+float f1 , f2;
+f2 = d1 = 0.25;
+d2 = f1 = 0.25;
+
+if(f1 == f2)
+{
+  printf("f1 = f2\n");
+}
+if(f1 != f2)
+{
+  printf("f1 != f2\n");
+}
+if(d1 == d2)
+{
+  printf("d1 = d2\n");
+}
+if(d1 != d2)
+{
+  printf("d1 != d2\n");
+}
+
+//printf("p1\n");
   return 0;
 }
