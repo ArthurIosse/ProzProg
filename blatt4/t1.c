2cf00a8df9e4b5248f7c91378eaa8e2a79092cd4
Arthur <iosse.arthur@gmail.com>
2018-11-18 20:21:06 +0100

    t1 sin func

--- blatt4/t1.c
+++ blatt4/t1.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
 double sin(double x)
 {
-x *= 10;
+  double c = x;
+  double y = c;
+  double yalt = y + 1.0;
 
-  return x;
+
+  for (int k = 1 ; y != yalt ; k++)
+  {
+    c=-c*(x*x)/((2*k+1)*(2*k));
+    yalt=y;
+    y=y+c;
+  }
+
+  return y;
 }
 
 
 int main (int argc, char * argv[])
 {
 
-printf("%d\n",sin(12.0));
+printf("%f\n",sin(12.0));
 
   return 0;
 }
