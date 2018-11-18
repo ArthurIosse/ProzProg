5ee40cb8a80745d9ee2d65e07aae04d3b4425327
Arthur <iosse.arthur@gmail.com>
2018-11-18 23:03:34 +0100

    t1 sin func

--- blatt4/t1.c
+++ blatt4/t1.c
@@ -1,26 +1,59 @@
 #include <stdio.h>
 #include <math.h>
 
-double sin(double x)
-{
-  double c = x;
-  double y = c;
-  double yalt = y + 1.0;
-  
-  for (int k = 1 ; y != yalt ; k++)
-  {
+//sinus Funktion
+double sin(double x){
+double c=x;
+double y=c;
+double yalt=y+1.0;
+
+for(int k=1; y!=yalt;k++){
     c=-c*(x*x)/((2*k+1)*(2*k));
     yalt=y;
     y=y+c;
-  }
-  return y;
+    //printf("%f\n",y);    //Zwischenergebnisse
+}
+return y;
+}
+//arctan Funktion
+double arctan(double x){
+double c=x;
+double y=c;
+double yalt=y+1.0;
+
+for(int k=1;y!=yalt;k++){
+    c=-c*(x*x*(2*k-1))/(2*k+1);
+    yalt=y;
+    y=y+c;
+    //printf("%f\n",y);     //Zwischenergebnisse
+}
+return y;
+}
+//ln Funktion
+double ln(double x){
+double c=(x-1);
+double y=c;
+double yalt=y+1.0;
+
+for(int k=2; yalt!=y;k++){
+    c=-c*((x-1)*(k-1))/k;
+    yalt=y;
+    y=y+c;
+    //printf("%f\n",y);     //Zwischenergebnisse
+
+}
+
+return y;
+
 }
 
 
 int main (int argc, char * argv[])
 {
 
-printf("%f\n",sin(12.0));
+printf("sin  %f\n",sin(12.0));
+printf("arctan %f\n",arctan(0.3));
+printf("ln %f\n",ln(0.4));
 
   return 0;
 }
