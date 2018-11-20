de2b65ff8a9462637bdcd7d7ba36d210cabba0d8
Arthur <iosse.arthur@gmail.com>
2018-11-20 17:27:59 +0100

    exp

--- blatt4/p2.c
+++ blatt4/p2.c
@@ -15,8 +15,24 @@ for(int k=1; y!=yalt;k++){
 return y;
 }
 
+double mycos(double x){
+double c=1;
+double y=c;
+double yalt=y+1.0;
+
+for(int k=1; y!=yalt;k++){
+    c=-c*(x*x)/((2*k)*(2*k-1));
+    yalt=y;
+    y=y+c;
+    //printf("%f\n",y);    //Zwischenergebnisse
+}
+return y;
+}
+
 int main (int argc, char * argv[])
 {
-printf("%lf\n",myexp(8));
+printf("exp(8) = %lf\n",myexp(8));
+printf("cos(8) = %lf\n",mycos(8));
+
   return 0;
 }
