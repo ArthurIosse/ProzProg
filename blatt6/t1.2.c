485a080da4405abb7066209b2efbe3bed6649237
Arthur <iosse.arthur@gmail.com>
2018-12-06 20:33:42 +0100

    blat6

---
+++ blatt6/t1.2.c
@@ -0,0 +1,86 @@
+#include <math.h>
+#include <stdio.h>
+#include "libBMP.h"
+
+
+
+#define x_MIN -2.0
+#define x_MAX  2.0
+#define y_MIN -4.0
+#define y_MAX  4.0
+
+
+#define BMP_H 400
+#define BMP_W  500
+
+
+
+void toBMP(double x, double y, int* X, int* Y) {
+//wenn funkwert liegt auserhalb BMP
+  if ((x < x_MIN) || (x > x_MAX) || (y < y_MIN) || (y > y_MAX)) {
+    printf("Fehler: %f <= %f <= %f\n", x_MIN, x, x_MAX);
+    printf("   oder %f <= %f <= %f verletzt!\n", y_MIN, y, y_MAX);
+    *X = 0;
+    *Y = 0;
+    return;
+  }
+  *X = (int) ((x - x_MIN) * BMP_W / (x_MAX - x_MIN));
+  *Y = BMP_H - (int) ((y - y_MIN) * BMP_H / (y_MAX - y_MIN));
+
+}
+
+
+
+void toMath(int X, int Y, double* x, double* y) {
+//wenn funkwert liegt auserhalb BMP
+  if ((X < 0) || (X > BMP_W) || (Y < 0) || (Y > BMP_H)) {
+    printf("Fehler: 0 <= %d <= %d\n", X, BMP_W);
+    printf("   oder 0 <= %d <= %d verletzt!\n", Y, BMP_H);
+    *x = x_MIN;
+    *y = y_MIN;
+    return;
+  }
+  *x = x_MIN + ((double) X * (x_MAX - x_MIN)) / BMP_W;
+  *y = y_MIN + ((double) (BMP_H - Y) * (y_MAX - y_MIN)) / BMP_H;
+}
+
+
+
+
+int main() {
+  uint32_t* data = (uint32_t*) malloc (BMP_W * BMP_H * sizeof(uint32_t));
+
+  // Weisser Hintergrund
+  for (int X = 0; X < BMP_W; X++) {
+    for (int Y = 0; Y < BMP_H; Y++) {
+      data[Y * BMP_W + X] = COLOR_WHITE;
+    }
+  }
+
+  //  XY-Achsen
+  int XX, YY;
+
+  toBMP(0.0, 0.0, &XX, &YY);
+  //x
+  for (int X = 0; X < BMP_W; X++) {
+    data[YY * BMP_W + X] = COLOR_BLACK;
+  }
+  //y
+  for (int Y = 0; Y < BMP_H; Y++) {
+    data[Y * BMP_W + XX] = COLOR_BLACK;
+  }
+
+  // fk
+  for (int X = 0; X < BMP_W; X++) {
+    double x, y;
+    toMath(X, 0, &x, &y);
+    toBMP(x, x*x, &XX, &YY);
+    data[YY * BMP_W + X] = COLOR_RED;
+  }
+
+
+  bmp_create("plot.bmp", data, BMP_W, BMP_H);
+  free(data);
+
+  return 0;
+}
--- blatt6/t1.c
+++ blatt6/t1.c
@@ -1,9 +1,74 @@
+#include <math.h>
 #include <stdio.h>
+#include "libBMP.h"
+#define N 200
+#define M 120
+
+#define X_MIN 1
+#define X_MAX 17
+#define Y_MIN 10                    //Y_MAX und Y_MIN sind vertauscht
+#define Y_MAX -5
 
 
-int main(int argc, char * argv[])
-{
+void toMath(int X, int Y, int* x, int* y){
+
+    *x = X_MIN+((double)X*(X_MAX-X_MIN))/M;
+    *y = Y_MIN+((double)Y*(Y_MAX-Y_MIN))/N;
+
+}
+
+void toBMP(    ){     //tomath umgekehrt
+
+   // *X
+   // *Y
+
+}
 
 
-  return 0;
+
+
+int main() {
+
+    uint32_t* data = (uint32_t*) malloc (N*M*sizeof(uint32_t));
+
+    for (int i=0; i<M ;i++){
+
+        //x Achse
+        if(i==M/2){
+            for (int j=0; j<M ;j++){
+                    data[i*N+j] = COLOR_BLACK;
+        }
+        }else{
+
+        for (int j=0; j<N ;j++){
+            data[i*N + j] = COLOR_WHITE;
+        }
+        //y Achse
+        for (int j=0; j<M ;j++){
+            if(j==N/2)
+            data[i*N+j] = COLOR_BLACK;
+          }
+
+        }
+
+    }
+
+    for(int x=X_MIN; x<X_MAX; x++){
+        //for(int y=Y_MIN; y<Y_MAX; y++){
+        int y =x*x;
+        toMath(N,M,&x,&y);
+        data[x*N+y*y] = COLOR_RED;
+        }
+
+
+
+
+
+
+
+    bmp_create("plot.bmp", data, N, M);
+
+    free(data);
+
+    return 0;
 }
