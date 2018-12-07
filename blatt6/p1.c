4a9a6b02e6d20da62649f4bb25bdd0b7b8276450
Arthur <iosse.arthur@gmail.com>
2018-12-07 18:42:31 +0100

    presenz

--- blatt6/p1.c
+++ blatt6/p1.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 
 int main(int argc, char * argv[])
 {
+int * prim = (int*)malloc(100*sizeof(int));
+
+for (int i = 2 ; i <=100 ; i++)
+{
+  prim[i-2] = i;
+}
 
 
+for (int i = 2 ; i<=100 ; i++ )
+{
+  for(int j = i ; j<=98 ; j ++)
+  {
 
+    if(prim[j] % i == 0 )
+    {
+      prim[j] = 0 ;
+    }
+  }
+}
+
+for (int i = 0 ; i <=98 ; i++)
+{
+  printf("%d, ",prim[i]);
+}
+printf("\n");
+
+free(prim);
   return 0;
 }
---
+++ blatt6/p6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define mymac for(int i ; i<101; i++){printf("Hello World\n");}
+
+int main (int argc, char * argv[])
+{
+
+  
+mymac
+
+  return 0;
+}
